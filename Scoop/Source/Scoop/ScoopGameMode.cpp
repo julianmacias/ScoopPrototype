9b88e2fcbc81c13f21e0f61c5ebae3f68fde0316
Julian Macias <julianmaciasavila95@gmail.com>
2021-03-31 17:30:23 +0200

    ProjectOrganization

--- Scoop/Source/Scoop/ScoopGameMode.cpp
+++ Scoop/Source/Scoop/ScoopGameMode.cpp
@@ -7,7 +7,7 @@
 AScoopGameMode::AScoopGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/Mannequin/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
